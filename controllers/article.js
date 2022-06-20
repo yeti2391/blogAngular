0e7441ba3593e1423912d25d6e58fdd012367d3c
moi <marco2391@gmail.com>
2022-06-20 12:54:18 -0400

    se hace api para devolver todos los elementos dentro de la bd

--- controllers/article.js
+++ controllers/article.js
@@ -8,6 +8,11 @@
 
 */
 
+const validator = require('validator');
+const Article = require('../models/article');
+
+
+
 const controller = {
     datosCurso: (req, res)=>{
         return res.status(200).send({
@@ -21,6 +26,97 @@ const controller = {
         return res.status(200).send({
             message: 'Soy la accion test de mi controlador de articulos'
         });
+    },
+
+    save: (req, res)=>{
+        // Recoger parametros por post
+        const params = req.body;
+        
+        //validar datos(validator)
+        try {
+            //por alguna razon si cambio var por let/const no funciona
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+
+        } catch (err) {
+            return res.status(200).send({
+                status: 'error', 
+                message: 'Faltan datos por enviar!'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            // crear el objeto a guardar
+            const article = new Article();
+
+            // asignar valores
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
+
+            // guardar el articulos
+            article.save( (err, articleStored)=>{
+                if (err || !articleStored) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'El articulo no se ha guardado!'
+                    });
+                }
+                 // devolver respuesta
+                return res.status(200).send({
+                    status: 'success',
+                    article:articleStored
+                });
+               
+            });
+
+           
+        } else{
+            return res.status(200).send({
+                status: 'error',
+                message: 'Los datos no son validos!'
+            });
+        }
+        
+
+        /*
+        return res.status(200).send({
+            article: params
+        })
+        */
+    },
+
+    getArticles: (req, res)=>{
+        //query
+        let query = Article.find({});
+
+        const last = req.params.last; 
+        if(last || last != undefined){
+            query.limit(5);
+        }
+
+
+        //Find
+        query.sort('-_id').exec((err,articles)=>{
+            if(err){
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al devolver los articulos!'
+                });
+            }
+            if(!articles){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay articulos para mostrar!'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                articles
+            });
+        });
+
     }
     
 };
--- routes/article.js
+++ routes/article.js
@@ -5,8 +5,14 @@ const ArticleController = require('../controllers/article');
 
 const router = express.Router();
 
+
+//Rutas de pruebas
 router.get('/test-de-controlador', ArticleController.test);
 router.post('/datos-curso', ArticleController.datosCurso);
+
+//Rutas para articulos
+router.post('/save', ArticleController.save);
+router.get('/articles/:last?', ArticleController.getArticles);
 
 
 module.exports = router;

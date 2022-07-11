b3b36690b805227a1ed83d2f4240004a84221f49
moi <marco2391@gmail.com>
2022-07-11 12:43:35 -0400

    se termina de hacer rutas y navegacion. Tambien se agrega ejemplos de parametros por URL en pagina de prueba

--- blogAngular/src/app/app.routing.ts
+++ blogAngular/src/app/app.routing.ts
@@ -17,7 +17,8 @@ const appRoutes: Routes = [
     {path: 'blog', component: BlogComponent},
     {path: 'formulario', component: FormularioComponent},
     {path: 'peliculas', component: PeliculasComponent},
-    {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: 'pagina-de-pruebas', component: PaginaComponent}, //esto es la forma en angular para que el parametro sea opcional
+    {path: 'pagina-de-pruebas/:nombre/:apellido', component: PaginaComponent},
     {path: '**', component: ErrorComponent}
 ];
 
--- blogAngular/src/app/components/pagina/pagina.component.ts
+++ blogAngular/src/app/components/pagina/pagina.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-pagina',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina.component.css']
 })
 export class PaginaComponent implements OnInit {
+  //las propiedades publicas al final voy a poder mostrarlas en la vista ejemplo:
+  public nombre!: string;
+  public apellido!:string;
 
-  constructor() { }
+  constructor( private _route:ActivatedRoute, private _router:Router) { }
 
   ngOnInit(): void {
+    this._route.params.subscribe((params:Params)=>{
+      this.nombre = params['nombre'];
+      this.apellido = params['apellido'];
+    });
+  }
+
+  redireccion(){
+    this._router.navigate(['/pagina-de-pruebas', 'Marco', 'Gonzalez']);
   }
 
 }
+ 

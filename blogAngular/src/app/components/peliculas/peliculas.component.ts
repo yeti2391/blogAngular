0fdd96fc6edd3a05d5b5afb16ff377696af70d16
moi <marco2391@gmail.com>
2022-07-12 11:52:49 -0400

    se realizan pruebas de directivas if y for en peliculas

--- blogAngular/src/app/components/peliculas/peliculas.component.ts
+++ blogAngular/src/app/components/peliculas/peliculas.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peliculas.component.css']
 })
 export class PeliculasComponent implements OnInit {
+  
+  public peliculas:Array<any>;
 
-  constructor() { }
+  constructor() { 
+    this.peliculas=[
+      {title: 'Spiderman', image:'https://wallpapercave.com/dwp1x/OsrCGAo.jpg'},
+      {title: 'Avengers End Game', image:'https://images.wallpapersden.com/image/wxl-poster-of-avengers-endgame-movie_63830.jpg'},
+      {title: 'The batman', image:'https://www.soctex.com/wp-content/uploads/2021/07/The-Batman-2022.jpg'}
+    ];
+  }
 
   ngOnInit(): void {
+    console.log(this.peliculas);
   }
 
 }
